0923acb9b8fa49817355275169d3753dbd909b07
ArenRitz <aren.dolbakian@gmail.com>
2022-07-12 19:09:05 -0400

    Made login form disappear once signed in and re-appear once logged out

--- express_server.js
+++ express_server.js
@@ -47,7 +47,8 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"], };
+  res.render("urls_new", templateVars);
 });
 
 
@@ -55,7 +56,7 @@ app.post("/urls", (req, res) => {
   const id = generateRandomString();
   const longURL = req.body.longURL;
   addToDatabase(id, longURL);
-  console.log(urlDatabase);
+
   res.redirect(`/urls/${id}`); 
 });
 
