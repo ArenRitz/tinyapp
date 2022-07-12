c20965a8fb0e1159cdd13e364e95693e3afa6423
ArenRitz <aren.dolbakian@gmail.com>
2022-07-12 03:35:10 -0400

    Added function to add new url to db

--- express_server.js
+++ express_server.js
@@ -3,7 +3,6 @@ const generateRandomString = () => {
 }
 
 
-
 const express = require("express");
 const app = express();
 const PORT = 8080;
@@ -14,6 +13,10 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
+
+
 
 app.get("/", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -29,6 +32,14 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+
+app.post("/urls", (req, res) => {
+  const id = generateRandomString();
+  const longURL = req.body.longURL;
+  addToDatabase(id, longURL);
+  console.log(urlDatabase);
+  res.redirect(`/urls/${id}`); 
+});
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
