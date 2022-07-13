0829ba3d562f219c7b29b4599315b37bf81525dd
ArenRitz <aren.dolbakian@gmail.com>
2022-07-13 18:12:05 -0400

    Made so users can only see/edit/delete the links they add

--- express_server.js
+++ express_server.js
@@ -1,6 +1,18 @@
 // DataBase
 
 const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "admin",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+
+const test = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -15,6 +27,20 @@ const usersDb = {
 };
 
 // Helper Funcs
+
+const getUserDb = (cookie) => {
+  let currentDb = {};
+  let currentUser = cookie;
+  for (const urls in urlDatabase) {
+    if (urlDatabase[urls].userID === currentUser) {
+      let key = urls;
+      let value = urlDatabase[urls]["longURL"];
+      currentDb[key] = value;
+    }
+  }
+  return currentDb;
+}
+
 
 const generateRandomString = () => {
   return Math.random().toString(36).substring(2, 8);
@@ -51,16 +77,93 @@ const passwordCheck = (password) => {
   return false;
 }
 
+const getUrlbyId = (id, db) => {
+  if (Object.keys(db).includes(id)) {
+    return db[id];
+  }
+  return undefined;
+}
+
 // Settings
 const express = require("express");
 const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs");
 const cookieParser = require('cookie-parser');
-const e = require("express");
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
+// Get
+
+app.get("/urls", (req, res) => {
+  const state = getUserByCookie(req.cookies["user_id"])
+  if (!state) {
+    res.send("<h1>Please login to see or add links</h1>");
+  } else {
+    let currentDb = getUserDb(req.cookies["user_id"]);
+    const templateVars = { urls: currentDb, user: getUserByCookie(req.cookies["user_id"]) };
+    res.render("urls_index", templateVars);
+  }
+});
+
+app.get("/", (req, res) => {
+  let currentDb = getUserDb(req.cookies["user_id"]);
+  const templateVars = { urls: currentDb, user: getUserByCookie(req.cookies["user_id"]) };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  const state = getUserByCookie(req.cookies["user_id"])
+  if (!state) {
+    res.redirect("/login");
+  } else {
+    const templateVars = { user: getUserByCookie(req.cookies["user_id"]) };
+    res.render("urls_new", templateVars);
+  }
+});
+
+app.get("/urls/:id", (req, res) => {
+  const state = getUserByCookie(req.cookies["user_id"])
+  let currentDb = getUserDb(req.cookies["user_id"]);
+  if (!state) {
+    res.send("Please login to see or add links");
+  } else if (currentDb[req.params.id] === undefined) {
+    res.send("<h1>You don't have access to this link or it doesn't exist</h1>");
+  } else {
+    const templateVars = { id: req.params.id, longURL: currentDb[req.params.id], user: getUserByCookie(req.cookies["user_id"]) };
+    res.render("urls_show", templateVars);
+  }
+});
+
+app.get("/register", (req, res) => {
+  const state = getUserByCookie(req.cookies["user_id"])
+  if (state) {
+    res.redirect("/urls");
+  } else {
+    const templateVars = { user: state };
+    res.render("urls_register", templateVars);
+  }
+});
+
+app.get("/login", (req, res) => {
+  const state = getUserByCookie(req.cookies["user_id"])
+  if (state) {
+    res.redirect("/urls");
+  } else {
+    const templateVars = { user: state };
+    res.render("urls_login", templateVars)
+  }
+});
+
+app.get("/u/:id", (req, res) => {
+  let currentDb = getUserDb(req.cookies["user_id"]);
+  const longURL = getUrlbyId(req.params.id, currentDb);
+  if (!longURL) {
+    res.send("URL does not exist");
+  } else {
+    res.redirect(longURL);
+  }
+});
 
 // Post
 
@@ -94,15 +197,28 @@ app.post("/logout", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  const id = generateRandomString();
-  const longURL = req.body.longURL;
-  addLinkToDatabase(id, longURL);
-  res.redirect(`/urls/${id}`);
+  const state = getUserByCookie(req.cookies["user_id"])
+  if (!state) {
+    res.send("Please login to create a new URL");
+  } else {
+    const id = generateRandomString();
+    const longURL = req.body.longURL;
+    addLinkToDatabase(id, longURL);
+    res.redirect(`/urls/${id}`);
+  }
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id]
+  const state = getUserByCookie(req.cookies["user_id"])
+  let currentDb = getUserDb(req.cookies["user_id"]);
+  if (!state) {
+    res.send("Please login edit or delete links");
+  } else if (currentDb[req.params.id] === undefined) {
+    res.send("<h1>You don't have access to this link or it doesn't exist</h1>");
+  } else {
+  delete currentDb[req.params.id]
   res.redirect("/");
+  }
 });
 
 app.post("/urls/:id/update", (req, res) => {
@@ -110,42 +226,7 @@ app.post("/urls/:id/update", (req, res) => {
   res.redirect("/");
 });
 
-// Get
 
-app.get("/", (req, res) => {
-  const templateVars = { urls: urlDatabase, user: getUserByCookie(req.cookies["user_id"]) };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, user: getUserByCookie(req.cookies["user_id"]) };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  const templateVars = { user: getUserByCookie(req.cookies["user_id"]) };
-  res.render("urls_new", templateVars);
-});
-
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: getUserByCookie(req.cookies["user_id"]) };
-  res.render("urls_show", templateVars);
-});
-
-app.get("/register", (req, res) => {
-  const templateVars = { user: getUserByCookie(req.cookies["user_id"]) };
-  res.render("urls_register", templateVars);
-});
-
-app.get("/login", (req, res) => {
-  const templateVars = { user: getUserByCookie(req.cookies["user_id"]) };
-  res.render("urls_login", templateVars)
-});
-
-app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
-});
 
 // Port listener
 
